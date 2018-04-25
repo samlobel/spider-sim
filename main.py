21fdd87e0d3b44c61482abcacbdf10dbfec69ab0
Sam Lobel <samlobel@sas.upenn.edu>
2018-04-25 19:23:55 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Boom baby")
+
+print("importing bpy")
+import bpy
+print("bpy imported")
+
+print("importing numpy")
+import numpy
+print("numpy imported")
+
+asdf = numpy.asarray([1,2,3])
+print(asdf)
